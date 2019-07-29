9b13d96e5fd25d1beec4ea6e53b3eb5212fef206
Darin Webster <darinleenavy@gmail.com>
2019-07-29 21:59:55 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,5 @@
-def using_times
-  looping_string = "Wingardium Leviosa"
-  
-	p 7.times "#{looping_string}"
+def using_times(looping_string = "Wingardium Leviosa")
+  p 7.times "#{looping_string}"
 end
 
 
